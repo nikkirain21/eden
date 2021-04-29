355bfe1ab5a636d5a752d4dcdee37686d2d08697
nikkirain21 <48752672+nikkirain21@users.noreply.github.com>
2021-04-29 17:34:53 +0800

    Add files via upload

---
+++ lib/account.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class account extends StatefulWidget {
+  @override
+  _accountState createState() => _accountState();
+}
+
+class _accountState extends State<account> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User user;
+  bool isloggedin = false;
+
+  checkAuthentification() async {
+    _auth.authStateChanges().listen((user) {
+      if (user == null) {
+        Navigator.of(context).pushReplacementNamed("Start");
+      }
+    });
+  }
+
+  getUser() async {
+    User firebaseUser = _auth.currentUser;
+    await firebaseUser?.reload();
+    firebaseUser = _auth.currentUser;
+
+    if (firebaseUser != null) {
+      setState(() {
+        this.user = firebaseUser;
+        this.isloggedin = true;
+      });
+    }
+  }
+
+  signOut() async {
+    _auth.signOut();
+
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.checkAuthentification();
+    this.getUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: !isloggedin
+          ? CircularProgressIndicator()
+          : Column(
+              children: <Widget>[
+                SizedBox(height: 40.0),
+                Container(
+                  height: 300,
+                  child: Image(
+                    image: AssetImage("images/welcome.jpg"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "Hello ${user.displayName} you are Logged in as ${user.email} GALING MO SIS",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                RaisedButton(
+                  padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
+                  onPressed: signOut,
+                  child: Text('Signout',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                  color: Colors.orange,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                )
+              ],
+            ),
+    ));
+  }
+}
---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final Color themeColor1 = Color(0xff61b15a);
+final Color themeColor2 = Color(0xffadce74);
+final Color accentColor1 = Color(0xfffff76a);
+final Color accentColor2 = Color(0xffffce89);
+final Color textColor = Colors.black;
